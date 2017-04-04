9e0a564c1fff5d2c31e145c1320999e5e7837b77
Angel Rodriguez <arrmixer@gmail.com>
2017-04-04 11:51:33 -0400

    Initial commit

---
+++ src/com/AE/Main.java
@@ -0,0 +1,14 @@
+package com.AE;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        X x = new X(scanner.nextInt());
+        x.x();
+    }
+}
---
+++ src/com/AE/X.java
@@ -0,0 +1,18 @@
+package com.AE;
+
+/**
+ * Created by Angel on 1/11/17.
+ */
+public class X {
+    private int x;
+
+    public X(int x) {
+        this.x = x;
+    }
+
+    public void x(){
+        for(int x = 0;x<=12;x++){
+            System.out.println(x + " times " + this.x + " equals " + x * this.x);
+        }
+    }
+}
